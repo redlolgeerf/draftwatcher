cd417671e62a7d876519ce99cbadd5632f01940d
eyeinthebrick <mihanne@list.ru>
2014-05-19 22:47:43 +0400

    started adding user functionality

--- watcher/urls.py
+++ watcher/urls.py
@@ -10,4 +10,7 @@ urlpatterns = patterns('',
         url(r'^draft/(?P<draft_number>\d+-\d)/$', views.detail, name='detail'),
         url(r'^add_draft/$', views.add_draft, name='add_draft'),
         url(r'^update/$', views.update_drafts, name='update_drafts'),
+        url(r'^register/$', views.register, name='register'),
+        url(r'^logout/$', views.user_logout, name='user_logout'),
+        url(r'^login/$', views.user_login, name='user_login'),
         )

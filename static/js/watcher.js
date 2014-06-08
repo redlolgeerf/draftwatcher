0b50c1d0191b5133f7140c701dd2fd0614fe13d7
eyeinthebrick <mihanne@list.ru>
2014-06-08 18:37:52 +0400

    add button label change

---
+++ static/js/watcher.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('#addbtn').click(function(){
+        $("#addbtn").html('Пожалуйста, подождите')
+        alert( "Законопроект добавляется, попейте чаю")
+});
+    $('#addbtn2').click(function(){
+        $("#addbtn2").html('Пожалуйста, подождите')
+        alert( "Законопроект добавляется, попейте чаю")
+});
+});

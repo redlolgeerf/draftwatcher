b7c8854eab2ba237e0bf96c346bc98810d5752db
eyeinthebrick <mihanne@list.ru>
2014-06-09 23:35:42 +0400

    got rid of tea

--- static/js/watcher.js
+++ static/js/watcher.js
@@ -1,10 +1,8 @@
 $(document).ready(function() {
     $('#addbtn').click(function(){
         $("#addbtn").html('Пожалуйста, подождите')
-        alert( "Законопроект добавляется, попейте чаю")
 });
     $('#addbtn2').click(function(){
         $("#addbtn2").html('Пожалуйста, подождите')
-        alert( "Законопроект добавляется, попейте чаю")
 });
 });

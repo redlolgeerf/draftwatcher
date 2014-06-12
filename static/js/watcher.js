6945c0e1173a36bda76a39e0cc4e75b77e7d0f43
eyeinthebrick <mihanne@list.ru>
2014-06-12 18:06:53 +0400

    added javascript for comment form

--- static/js/watcher.js
+++ static/js/watcher.js
@@ -5,4 +5,11 @@ $(document).ready(function() {
     $('#addbtn2').click(function(){
         $("#addbtn2").html('Пожалуйста, подождите')
 });
+    $('#editcomment').click(function(){
+        $('#id_comment').prop("disabled", false);
+        var r= $('<button id="add_comment" class="btn btn-xs btn-primary" type="submit" name="submit">Сохранить</button>');
+        $("#add_comment_form").append(r);
+        $('#editcomment').hide();
+        
+});
 });
